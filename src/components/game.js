9ca31b552dc2e1d2e7f54da3fa8e8b4cc1933c5f
Peter Ramos <peterdramos02@gmail.com>
2019-01-29 12:50:37 -0500

    connect components to redux with the connect and dispatch

--- src/components/game.js
+++ src/components/game.js
@@ -17,40 +17,6 @@ class Game extends React.Component {
     });
   }
 
-  makeGuess(guess) {
-    guess = parseInt(guess, 10);
-    if (isNaN(guess)) {
-      this.setState({ feedback: 'Please enter a valid number' });
-      return;
-    }
-
-    const difference = Math.abs(guess - this.state.correctAnswer);
-
-    let feedback;
-    if (difference >= 50) {
-      feedback = 'You\'re Ice Cold...';
-    } else if (difference >= 30) {
-      feedback = 'You\'re Cold...';
-    } else if (difference >= 10) {
-      feedback = 'You\'re Warm.';
-    } else if (difference >= 1) {
-      feedback = 'You\'re Hot!';
-    } else {
-      feedback = 'You got it!';
-    }
-
-    this.setState({
-      feedback,
-      guesses: [...this.state.guesses, guess]
-    });
-
-    // We typically wouldn't touch the DOM directly like this in React
-    // but this is the best way to update the title of the page,
-    // which is good for giving screen-reader users
-    // instant information about the app.
-    document.title = feedback ? `${feedback} | Hot or Cold` : 'Hot or Cold';
-  }
-
   generateAuralUpdate() {
     const { guesses, feedback } = this.state;
 
@@ -69,20 +35,20 @@ class Game extends React.Component {
   }
 
   render() {
-    const { feedback, guesses, auralStatus } = this.state;
+    const { feedback, guesses, auralStatus } = this.props;
     const guessCount = guesses.length;
-
+    // document.title = feedback ? `${feedback} | Hot or Cold` : 'Hot or Cold';
     return (
       <div>
         <Header
-          onRestartGame={() => this.restartGame()}
+          onRestartGame={() => this.props.dispatch(resetGame())}
           onGenerateAuralUpdate={() => this.generateAuralUpdate()}
         />
         <main role="main">
           <GuessSection
             feedback={feedback}
             guessCount={guessCount}
-            onMakeGuess={guess => this.makeGuess(guess)}
+            onMakeGuess={guess => this.props.dispatch(addGuess(guess))}
           />
           <StatusSection guesses={guesses} 
             auralStatus={auralStatus}
